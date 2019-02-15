239e6fa2abc4b108c28ecdde25202e1fa5584d16
Marta Sajdak <crappylime@gmail.com>
2019-02-15 18:09:01 +0100

    Export ReactiveFormsModule in shared

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -10,15 +9,7 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    TranslateModule,
-    SharedModule,
-    FlexLayoutModule,
-    MaterialModule,
-    LoginRoutingModule
-  ],
+  imports: [CommonModule, TranslateModule, SharedModule, FlexLayoutModule, MaterialModule, LoginRoutingModule],
   declarations: [LoginComponent]
 })
 export class LoginModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '@app/material.module';
 import { AgGridModule } from 'ag-grid-angular';
 import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  imports: [FlexLayoutModule, MaterialModule, AgGridModule.withComponents([]), CommonModule],
+  imports: [FlexLayoutModule, MaterialModule, AgGridModule.withComponents([]), CommonModule, ReactiveFormsModule],
   declarations: [LoaderComponent],
-  exports: [LoaderComponent]
+  exports: [LoaderComponent, ReactiveFormsModule]
 })
 export class SharedModule {}

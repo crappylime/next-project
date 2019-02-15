c6be3a8d2499081bcb5ca66404589205cdec9027
Marta Sajdak <crappylime@gmail.com>
2019-02-15 14:35:58 +0100

    Generate missing components for search module

---
+++ src/app/pages/search/components/search-table/search-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-table',
+  templateUrl: './search-table.component.html',
+  styleUrls: ['./search-table.component.scss']
+})
+export class SearchTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -6,9 +6,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@app/material.module';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchContainerComponent } from './components/search-container/search-container.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchTableComponent } from './components/search-table/search-table.component';
+import { ProfileSidebarComponent } from './components/profile-sidebar/profile-sidebar.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, SearchRoutingModule],
-  declarations: [SearchContainerComponent]
+  declarations: [SearchContainerComponent, SearchFormComponent, SearchTableComponent, ProfileSidebarComponent]
 })
 export class SearchModule {}
